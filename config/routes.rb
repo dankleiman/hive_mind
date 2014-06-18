dd0ad76d96df825a28cf70a1828e6fbece39efca
Dan Kleiman <info@dankleiman.com>
2014-06-17 22:29:17 -0400

    Added github omniauth, part 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :achievements, only: [:index, :create, :new]
 
   root 'achievements#index'
+
+  match 'auth/github/callback', to: 'sessions#create', via: [:get, :post]
 end
