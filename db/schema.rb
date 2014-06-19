1b34be2e268d195c998d4c4ca70d8ac9d86fe061
Dan Kleiman <info@dankleiman.com>
2014-06-19 16:38:29 -0400

    Added role column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618202019) do
+ActiveRecord::Schema.define(version: 20140619202712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140618202019) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role",       default: "user"
   end
 
   create_table "votes", force: true do |t|
